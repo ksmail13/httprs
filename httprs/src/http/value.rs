7e49a2a1eface6c86c494c3f2b165b8692c0a409
ksmail13 <ksmail13@gmail.com>
2026-03-18 01:33:12 +0900

    response status code 최적화
    - string 런타임 생성 대신 macro를 활용해 static str 을 사용하도록 수정

--- httprs/src/http/value.rs
+++ httprs/src/http/value.rs
@@ -79,71 +79,62 @@ impl Display for HttpMethod {
     }
 }
 
-#[allow(dead_code)]
-pub enum HttpResponseCode {
-    Ok,
-    Created,
-    Accepted,
-    NoContent,
-    MovedPermanetly,
-    MovedTemporarily,
-    NotModified,
-    BadRequest,
-    Unauthorized,
-    Forbidden,
-    NotFound,
-    InternalServerError,
-    NotImplemented,
-    BadGateway,
-    ServiceUnavailable,
-}
+macro_rules! define_http_response_code {
+    (
+        $(
+            $name:ident = $code:expr, $reason:expr
+        ),* $(,)?
+    ) => {
+        #[allow(dead_code)]
+        #[derive(Debug, Clone, PartialEq, Eq)]
+        pub enum HttpResponseCode {
+            $( $name ),*
+        }
 
-impl HttpResponseCode {
-    pub fn code(&self) -> i32 {
-        return match self {
-            HttpResponseCode::Ok => 200,
-            HttpResponseCode::Created => 201,
-            HttpResponseCode::Accepted => 202,
-            HttpResponseCode::NoContent => 204,
-            HttpResponseCode::MovedPermanetly => 301,
-            HttpResponseCode::MovedTemporarily => 302,
-            HttpResponseCode::NotModified => 304,
-            HttpResponseCode::BadRequest => 400,
-            HttpResponseCode::Unauthorized => 401,
-            HttpResponseCode::Forbidden => 403,
-            HttpResponseCode::NotFound => 404,
-            HttpResponseCode::InternalServerError => 500,
-            HttpResponseCode::NotImplemented => 501,
-            HttpResponseCode::BadGateway => 502,
-            HttpResponseCode::ServiceUnavailable => 503,
-        };
-    }
+        impl HttpResponseCode {
+            pub fn code(&self) -> i32 {
+                match self {
+                    $( HttpResponseCode::$name => $code ),*
+                }
+            }
 
-    pub fn reason(&self) -> &str {
-        return match self {
-            HttpResponseCode::Ok => "OK",
-            HttpResponseCode::Created => "Created",
-            HttpResponseCode::Accepted => "Accepted",
-            HttpResponseCode::NoContent => "No Content",
-            HttpResponseCode::MovedPermanetly => "Moved Permanently",
-            HttpResponseCode::MovedTemporarily => "Moved Temporarily",
-            HttpResponseCode::NotModified => "Not Modified",
-            HttpResponseCode::BadRequest => "Bad Request",
-            HttpResponseCode::Unauthorized => "Unauthorized",
-            HttpResponseCode::Forbidden => "Forbidden",
-            HttpResponseCode::NotFound => "NotFound",
-            HttpResponseCode::InternalServerError => "Interna Server Error",
-            HttpResponseCode::NotImplemented => "Not Implemented",
-            HttpResponseCode::BadGateway => "Bad Gateway",
-            HttpResponseCode::ServiceUnavailable => "Service Unavailable",
-        };
+            pub fn reason(&self) -> &'static str {
+                match self {
+                    $( HttpResponseCode::$name => $reason ),*
+                }
+            }
+
+            pub fn as_str(&self) -> &'static str {
+                match self {
+                    $( HttpResponseCode::$name => concat!(stringify!($code), " ", $reason) ),*
+                }
+            }
+        }
+
+        impl Display for HttpResponseCode {
+            fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+                f.write_str(self.as_str())
+            }
+        }
     }
 }
 
-impl Display for HttpResponseCode {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        return f.write_fmt(format_args!("{} {}", self.code(), self.reason()));
-    }
+define_http_response_code! {
+    Ok = 200, "OK",
+    Created = 201, "Created",
+    Accepted = 202, "Accepted",
+    NoContent = 204, "No Content",
+    MovedPermanetly = 301, "Moved Permanently",
+    MovedTemporarily = 302, "Moved Temporarily",
+    NotModified = 304, "Not Modified",
+    BadRequest = 400, "Bad Request",
+    Unauthorized = 401, "Unauthorized",
+    Forbidden = 403, "Forbidden",
+    NotFound = 404, "Not Found",
+    InternalServerError = 500, "Internal Server Error",
+    NotImplemented = 501, "Not Implemented",
+    BadGateway = 502, "Bad Gateway",
+    ServiceUnavailable = 503, "Service Unavailable",
 }
 
 #[allow(dead_code)]
